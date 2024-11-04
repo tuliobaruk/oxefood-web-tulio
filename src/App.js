aee1f95fb9b99f8fc867d34f0bcd5c2f99e6a9d2
tuliobaruk <tuliobarukms@gmail.com>
2024-11-04 10:57:48 -0300

    feat: Implementar rotas utilizando BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import { Segment } from 'semantic-ui-react';
 import './App.css';
-import Home from './views/home/Home';
-import FormCliente from './views/cliente/FormCliente';
-import FormEntregador from './views/entregador/FormEntregador';
+import Rotas from './Rotas';
 
 function App() {
   return (
     <div className="App">
-      <FormEntregador />
+      <Rotas />
 
       <div style={{ marginTop: '6%' }}>
         <Segment vertical color='grey' size='tiny' textAlign='center'>
