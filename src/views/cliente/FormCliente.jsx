5110ca59b7ac794c4357ae9dfdd3104a9fd47946
tuliobaruk <tuliobarukms@gmail.com>
2024-12-02 12:09:19 -0300

    feat: uso do react-toastify em form-cliente

--- src/views/cliente/FormCliente.jsx
+++ src/views/cliente/FormCliente.jsx
@@ -4,6 +4,7 @@ import InputMask from 'react-input-mask';
 import MenuSistema from '../../MenuSistema';
 import { Link } from "react-router-dom";
 import { Button, Container, Divider, Form, Icon } from 'semantic-ui-react';
+import { notifyError, notifySuccess } from '../util/Util';
 
 export default function FormCliente() {
 
@@ -25,10 +26,16 @@ export default function FormCliente() {
 
         axios.post("http://localhost:8080/api/cliente", clienteRequest)
             .then((response) => {
-                console.log('Cliente cadastrado com sucesso.')
+                notifySuccess('Cliente cadastrado com sucesso.')
             })
             .catch((error) => {
-                console.log('Erro ao incluir o um cliente.')
+                if (error.response.data.errors != undefined) {
+                    for (let i = 0; i < error.response.data.errors.length; i++) {
+                        notifyError(error.response.data.errors[i].defaultMessage)
+                    }
+                } else {
+                    notifyError(error.response.data.message)
+                }
             })
     }
 
